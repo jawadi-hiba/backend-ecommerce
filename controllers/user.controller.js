f025f500b133061351a830b4b472636568ee2b43
jawadi-hiba <hibajawadi88@gmail.com>
2024-07-03 13:45:37 +0200

    last update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,21 @@
 export const getUsers = (req, res) => {
-    res.json({ name: "userName" });
-  };
+  res.json({ name: "userName" });
+};
+
+export const allAccess = (req, res) => {
+  res.status(200).send("public content .");
+};
+
+export const userBoard = (req, res) => {
+  res.status(200).send("User content.");
+};
+
+export const adminBoard = (req, res) => {
+
+  res.status(200).send("admin content.");
+};
+
+export const superAdminBoard = (req, res) => {
+  res.status(200).send("superAdmin content.");
+};
+
